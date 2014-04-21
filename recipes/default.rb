9c9f924bbf026bfaab8cb71650f80909f7662b55
Ryan Schlesinger <ryan@instanceinc.com>
2014-04-21 10:41:10 -0700

    Install yum plugin at compile time

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,17 +1,21 @@
-remote_file "/tmp/yum-plugin-s3-iam-#{node['yum-s3-iam']['rpm_version']}.noarch.rpm" do
+file = remote_file "/tmp/yum-plugin-s3-iam-#{node['yum-s3-iam']['rpm_version']}.noarch.rpm" do
   source node['yum-s3-iam']['rpm_url']
-  action :create_if_missing
+  action :nothing
 
   not_if do
     system("rpm -q yum-plugin-s3-iam | grep -q 'yum-plugin-s3-iam-#{node['yum-s3-iam']['rpm_version']}.noarch'")
   end
 end
 
-rpm_package 'yum-plugin-s3-iam' do
-  action :install
+file.run_action(:create_if_missing)
+
+package = rpm_package 'yum-plugin-s3-iam' do
+  action :nothing
   source "/tmp/yum-plugin-s3-iam-#{node['yum-s3-iam']['rpm_version']}.noarch.rpm"
 
   not_if do
     system("rpm -q yum-plugin-s3-iam | grep -q 'yum-plugin-s3-iam-#{node['yum-s3-iam']['rpm_version']}.noarch'")
   end
 end
+
+package.run_action(:install)
